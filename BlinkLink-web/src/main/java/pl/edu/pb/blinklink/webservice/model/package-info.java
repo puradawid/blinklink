ee9d0e3eed004a29e33bff6dc6e3a9d6ddd1e98b
Dawid Pura <puradawid@gmail.com>
2014-07-29 20:37:42 +0200

    Package info for JAXB annotations added.

---
+++ BlinkLink-web/src/main/java/pl/edu/pb/blinklink/webservice/model/package-info.java
@@ -0,0 +1,8 @@
+
+@XmlSchema(
+		namespace = "http://webservice.blinklink.pb.edu.pl",
+		elementFormDefault = XmlNsForm.QUALIFIED,
+		xmlns={@XmlNs(prefix="ns0", namespaceURI="http://webservice.blinklink.pb.edu.pl")})
+package pl.edu.pb.blinklink.webservice.model;
+
+import javax.xml.bind.annotation.*;
